6e27924f5d37b18edadb1e96638505643e706768
adzkar <adzkarfauzie02@gmail.com>
2018-12-01 10:32:34 +0700

    Add Some DAO Test Artikel

--- src/test/java/com/forum/inf/dao/TestArtikel.java
+++ src/test/java/com/forum/inf/dao/TestArtikel.java
@@ -35,7 +35,7 @@ public class TestArtikel {
     @Autowired
     private JpaTransactionManager jtm;
     
-    @Test
+//    @Test
     public void testInsertArtikel() throws SQLException {
         User u = new User();
         u.setNama("Tuyul");
@@ -67,4 +67,44 @@ public class TestArtikel {
         }
         
     }
+    
+//    @Test
+    public void testTambahArtikel() throws SQLException {
+        Artikel a = new Artikel();
+        a.setImage("Gambar boi");
+        a.setIsi("contennya ceritanya");
+        a.setJudul("Hola Dunia");
+        
+        ad.save(a);
+        
+        ds = jtm.getDataSource();
+        
+        try(Connection c = ds.getConnection()) {
+            String sql = "select count(*) as jumlah from tb_artikel where judul_artikel='Hola Dunia'";
+            ResultSet rs = c.createStatement().executeQuery(sql);
+            Assert.assertTrue(rs.next());
+            Assert.assertEquals(1L, rs.getLong("jumlah"));
+        }
+        
+    }
+    
+    @Test
+    public void tambahArtikelId() throws SQLException {
+        Artikel a = new Artikel();
+        a.setImage("Gambar boi");
+        a.setIsi("adfsd");
+        a.setJudul("Hola World");
+        
+        String id = ad.save(a).getId();
+        
+        ds = jtm.getDataSource();
+        
+        try(Connection c = ds.getConnection()) {
+            String sql = "select count(*) as jumlah from tb_artikel where id_artikel='"+id+"'";
+            ResultSet rs = c.createStatement().executeQuery(sql);
+            Assert.assertTrue(rs.next());
+            Assert.assertEquals(1L, rs.getLong("jumlah"));
+        }
+    }
+    
 }
